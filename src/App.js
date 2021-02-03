d998a99a8d1c7d7d230ff41db010c7c42d226422
gorushkin <gorushkin@gmail.com>
2021-02-03 20:41:45 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
-const clickHandler = () => {
-  console.log('click');
-  throw new Error('Alarm!!!');
-};
 
 const methodDoesNotExist = () => {
   throw new Error('Alarm!!!');
 };
 
 function App() {
-  // return (
-  //   <div className="App">
-  //     <button onClick={clickHandler} >Click</button>
-  //   </div>
-  // );
   return <button onClick={methodDoesNotExist}>Break the world</button>;
 }
 
