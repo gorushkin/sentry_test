fb7ddeb2c7c211d36891cb08e6e043e97b24b25e
gorushkin <gorushkin@gmail.com>
2021-02-04 10:53:50 +0300

    removed Integrations sentry

--- src/index.js
+++ src/index.js
@@ -8,12 +8,6 @@ import App from './App';
 
 Sentry.init({
   dsn: "https://755bccd782164d09812c03648c0ef777@o515086.ingest.sentry.io/5621749",
-  autoSessionTracking: true,
-  integrations: [new Integrations.BrowserTracing()],
-
-  // We recommend adjusting this value in production, or using tracesSampler
-  // for finer control
-  tracesSampleRate: 1.0,
 });
 
 ReactDOM.render(
