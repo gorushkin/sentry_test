745995d3d726ef2017d280f055233170052259dc
gorushkin <gorushkin@gmail.com>
2021-02-05 13:50:15 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
-
+import * as Sentry from '@sentry/react';
 
 const methodDoesNotExist = () => {
   throw new Error('Alarm!!!');
 };
 
-function App() {
-  return <button onClick={methodDoesNotExist}>Break the world</button>;
+function FallbackComponent() {
+  return <div>An error has occurred</div>;
 }
+
+const myFallback = <FallbackComponent />;
+
+const Btn = () => <button onClick={methodDoesNotExist}>Break the world</button>;
+
+const App = () => (
+  <Sentry.ErrorBoundary fallback={myFallback} showDialog onError={() => console.log('error')}>
+    <Btn />
+  </Sentry.ErrorBoundary>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,23 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import * as Sentry from '@sentry/react';
+import * as Sentry from "@sentry/react";
 import App from './App';
 
 Sentry.init({
-  dsn: 'https://755bccd782164d09812c03648c0ef777@o515086.ingest.sentry.io/5621749',
+  dsn: "https://755bccd782164d09812c03648c0ef777@o515086.ingest.sentry.io/5621749",
 });
 
-function FallbackComponent() {
-  return <div>An error has occurred</div>;
-}
-
-const myFallback = <FallbackComponent />;
-
 ReactDOM.render(
-  <Sentry.ErrorBoundary fallback={myFallback}>
+  <React.StrictMode>
     <App />
-  </Sentry.ErrorBoundary>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
